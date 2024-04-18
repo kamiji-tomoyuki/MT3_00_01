b03ee0113d1867864b16befc49507882d01bcb0b
kamiji-tomoyuki <k023g0115@g.neec.ac.jp>
2024-04-18 20:44:39 +0900

    MT3_01_03_確認課題

--- main.cpp
+++ main.cpp
@@ -1,54 +1,72 @@
 #include <Novice.h>
-#include <math.h>
+#include "Vector3.h"
+#include "assert.h"
 
 const char kWindowTitle[] = "LE2B_07_カミジ_トモユキ";
 
-
-struct Vector3 {
-
-	float x;
-	float y;
-	float z;
-
+struct Matrix4x4
+{
+	float m[4][4];
 };
 
-//加算
-Vector3 Add(const Vector3& v1, const  Vector3& v2) {
-	Vector3 result = { v1.x + v2.x,v1.y + v2.y, v1.z + v2.z };
+//1. 平行移動行列
+Matrix4x4 MakeTranslateMatrix(const Vector3& translate)
+{
+	Matrix4x4 result = {
+		1.0f,0.0f,0.0f,0.0f,
+		0.0f,1.0f,0.0f,0.0f,
+		0.0f,0.0f,1.0f,0.0f,
+		translate.x,translate.y,translate.z,1.0f
+	};
 	return result;
 }
-//減算
-Vector3 Subtract(const Vector3& v1, const  Vector3& v2) {
-	Vector3 result = { v1.x - v2.x,v1.y - v2.y, v1.z - v2.z };
+//2. 拡大縮小行列
+Matrix4x4 MakeScaleMatrix(const Vector3& scale)
+{
+	Matrix4x4 result = {
+		scale.x,0.0f,0.0f,0.0f,
+		0.0f,scale.y,0.0f,0.0f,
+		0.0f,0.0f,scale.z,0.0f,
+		0.0f,0.0f,0.0f,1.0f,
+	};
 	return result;
 }
-//スカラー倍
-Vector3 Multiply(float scalar, const  Vector3& v) {
-	Vector3 result = { scalar * v.x,scalar * v.y, scalar * v.z };
-	return result;
-}
-//内積
-float Dot(const Vector3& v1, const  Vector3& v2) {
-	float result = v1.x * v2.x + v1.y * v2.y + v1.z * v2.z;
-	return result;
-}
-//長さ(ノルム)
-float Length(const Vector3& v) {
-	float result = sqrtf(v.x * v.x + v.y * v.y + v.z * v.z);
-	return result;
-}
-//正規化
-Vector3 Normalize(const Vector3& v) {
-	Vector3 result = { v.x / Length(v),v.y / Length(v), v.z / Length(v) };
+//3. 座標変換
+Vector3 Transform(const Vector3& vector, const Matrix4x4& matrix)
+{
+	Vector3 result;
+	result.x = vector.x * matrix.m[0][0] + vector.y * matrix.m[1][0] + vector.z * matrix.m[2][0] + 1.0f * matrix.m[3][0];
+	result.y = vector.x * matrix.m[0][1] + vector.y * matrix.m[1][1] + vector.z * matrix.m[2][1] + 1.0f * matrix.m[3][1];
+	result.z = vector.x * matrix.m[0][2] + vector.y * matrix.m[1][2] + vector.z * matrix.m[2][2] + 1.0f * matrix.m[3][2];
+	float w = vector.x * matrix.m[0][3] + vector.y * matrix.m[1][3] + vector.z * matrix.m[2][3] + 1.0f * matrix.m[3][3];
+	assert(w != 0.0f);
+	result.x /= w;
+	result.y /= w;
+	result.z /= w;
 	return result;
 }
 
-//数値表示
+
+static const int kRowHeight = 20;
 static const int kColumnWidth = 60;
-void VectorScreenPrintf(int x, int y, const Vector3& vec, const char* label) {
-	Novice::ScreenPrintf(x, y, "%.02f", vec.x);
-	Novice::ScreenPrintf(x + kColumnWidth, y, "%.02f", vec.y);
-	Novice::ScreenPrintf(x + kColumnWidth * 2, y, "%.02f", vec.z);
+
+
+void MatrixScreenPrintf(int x, int y, const Matrix4x4& matrix)
+{
+	for (int row = 0; row < 4; ++row)
+	{
+		for (int column = 0; column < 4; ++column)
+		{
+			Novice::ScreenPrintf(x + column * kColumnWidth, y + row * kRowHeight, "%6.02f", matrix.m[row][column]);
+		}
+	}
+}
+
+void VectorScreenPrintf(int x, int y, const Vector3& vector, const char* label)
+{
+	Novice::ScreenPrintf(x, y, "%.02f", vector.x);
+	Novice::ScreenPrintf(x + kColumnWidth, y, "%.02f", vector.y);
+	Novice::ScreenPrintf(x + kColumnWidth * 2, y, "%.02f", vector.z);
 	Novice::ScreenPrintf(x + kColumnWidth * 3, y, "%s", label);
 }
 
@@ -58,19 +76,19 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	// ライブラリの初期化
 	Novice::Initialize(kWindowTitle, 1280, 720);
 
-	Vector3 v1{ 1.0f,3.0f,-5.0f };
-	Vector3 v2{ 4.0f,-1.0f,2.0f };
-	float k = { 4.0f };
+	Vector3 translate(4.1f, 2.6f, 0.8f);
+	Vector3 scale{ 1.5f,5.2f,7.3f };
+	Matrix4x4 translateMatrix = MakeTranslateMatrix(translate);
+	Matrix4x4 scaleMatrix = MakeScaleMatrix(scale);
+	Vector3 point{ 2.3f,3.8f,1.4f };
+	Matrix4x4 transformMatrix = {
+		1.0f,2.0f,3.0f,4.0f,
+		3.0f,1.0f,1.0f,2.0f,
+		1.0f,4.0f,2.0f,3.0f,
+		2.0f,2.0f,1.0f,3.0f,
+	};
+	Vector3 transformed = Transform(point, transformMatrix);
 
-
-	Vector3 resultAdd = Add(v1, v2);//加算
-	Vector3 resultSubtract = Subtract(v1, v2);//減算
-	Vector3 resultMultiply = Multiply(k, v1);//スカラー倍
-	float resultDot = Dot(v1, v2);//内積
-	float resultLength = Length(v1);//長さ(ノルム)
-	Vector3 resultNormalize = Normalize(v2);//正規化
-
-	int kRowHeight = 20;
 
 	// キー入力結果を受け取る箱
 	char keys[256] = { 0 };
@@ -97,17 +115,9 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		/// ↓描画処理ここから
 		///
 
-		VectorScreenPrintf(0, 0, resultAdd, " : Add");
-		VectorScreenPrintf(0, kRowHeight, resultSubtract, " : Subtract");
-		VectorScreenPrintf(0, kRowHeight * 2, resultMultiply, " : Multiply");
-		Novice::ScreenPrintf(0, kRowHeight * 3, "%.02f : Dot", resultDot);
-		Novice::ScreenPrintf(0, kRowHeight * 4, "%.02f : Length", resultLength);
-		VectorScreenPrintf(0, kRowHeight * 5, resultNormalize, " : Normalize");
-
-
-
-
-
+		VectorScreenPrintf(0, 0, transformed, "transformed");
+		MatrixScreenPrintf(0, kRowHeight + 20, translateMatrix);
+		MatrixScreenPrintf(0, kRowHeight * 5 + 40, scaleMatrix);
 
 
 		///
