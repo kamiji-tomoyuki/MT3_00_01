6f6daa05832eb745f53bea61cbc6f243a523def2
kamiji-tomoyuki <k023g0115@g.neec.ac.jp>
2024-04-19 10:46:01 +0900

    保存用

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include <Novice.h>
 #include "Vector3.h"
 #include "assert.h"
+#define _USE_MATH_DEFINES
+#include <cmath>
+
 
 const char kWindowTitle[] = "LE2B_07_カミジ_トモユキ";
 
@@ -9,7 +12,7 @@ struct Matrix4x4
 	float m[4][4];
 };
 
-//1. 平行移動行列
+//平行移動行列
 Matrix4x4 MakeTranslateMatrix(const Vector3& translate)
 {
 	Matrix4x4 result = {
@@ -20,7 +23,7 @@ Matrix4x4 MakeTranslateMatrix(const Vector3& translate)
 	};
 	return result;
 }
-//2. 拡大縮小行列
+//拡大縮小行列
 Matrix4x4 MakeScaleMatrix(const Vector3& scale)
 {
 	Matrix4x4 result = {
@@ -31,7 +34,105 @@ Matrix4x4 MakeScaleMatrix(const Vector3& scale)
 	};
 	return result;
 }
-//3. 座標変換
+//回転行列3x3
+Matrix4x4 MakeRotateZMatrix(float radian) {
+
+	Matrix4x4 result;
+
+	result.m[0][0] = std::cos(radian);
+	result.m[0][1] = std::sin(radian);
+	result.m[0][2] = 0;
+	result.m[0][3] = 0;
+
+	result.m[1][0] = -std::sin(radian);
+	result.m[1][1] = std::cos(radian);
+	result.m[1][2] = 0;
+	result.m[1][3] = 0;
+
+	result.m[2][0] = 0;
+	result.m[2][1] = 0;
+	result.m[2][2] = 1;
+	result.m[2][3] = 0;
+
+	result.m[3][0] = 0;
+	result.m[3][1] = 0;
+	result.m[3][2] = 0;
+	result.m[3][3] = 1;
+
+	return result;
+}
+
+Matrix4x4 MakeRotateXMatrix(float radian) {
+
+	Matrix4x4 result;
+
+	result.m[0][0] = 1;
+	result.m[0][1] = 0;
+	result.m[0][2] = 0;
+	result.m[0][3] = 0;
+
+	result.m[1][0] = 0;
+	result.m[1][1] = std::cos(radian);
+	result.m[1][2] = std::sin(radian);
+	result.m[1][3] = 0;
+
+	result.m[2][0] = 0;
+	result.m[2][1] = -std::sin(radian);
+	result.m[2][2] = std::cos(radian);
+	result.m[2][3] = 0;
+
+	result.m[3][0] = 0;
+	result.m[3][1] = 0;
+	result.m[3][2] = 0;
+	result.m[3][3] = 1;
+
+	return result;
+}
+
+Matrix4x4 MakeRotateYMatrix(float radian) {
+
+	Matrix4x4 result;
+
+	result.m[0][0] = std::cos(radian);
+	result.m[0][1] = 0;
+	result.m[0][2] = -std::sin(radian);
+	result.m[0][3] = 0;
+
+	result.m[1][0] = 0;
+	result.m[1][1] = 1;
+	result.m[1][2] = 0;
+	result.m[1][3] = 0;
+
+	result.m[2][0] = std::sin(radian);
+	result.m[2][1] = 0;
+	result.m[2][2] = std::cos(radian);
+	result.m[2][3] = 0;
+
+	result.m[3][0] = 0;
+	result.m[3][1] = 0;
+	result.m[3][2] = 0;
+	result.m[3][3] = 1;
+
+	return result;
+}
+
+
+Matrix4x4 Multiply(Matrix4x4& matrix1, Matrix4x4& matrix2) {
+
+	Matrix4x4 result;
+
+	for (int row = 0; row < 4; row++)
+	{
+		for (int column = 0; column < 4; column++)
+		{
+			result.m[row][column] = matrix1.m[row][0] * matrix2.m[0][column] + matrix1.m[row][1] * matrix2.m[1][column] + matrix1.m[row][2] * matrix2.m[2][column];
+		}
+	}
+	return result;
+};
+
+
+//座標変換
 Vector3 Transform(const Vector3& vector, const Matrix4x4& matrix)
 {
 	Vector3 result;
@@ -46,10 +147,10 @@ Vector3 Transform(const Vector3& vector, const Matrix4x4& matrix)
 	return result;
 }
 
+//描画用関数
 
 static const int kRowHeight = 20;
 static const int kColumnWidth = 60;
-
 
 void MatrixScreenPrintf(int x, int y, const Matrix4x4& matrix)
 {
@@ -76,7 +177,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	// ライブラリの初期化
 	Novice::Initialize(kWindowTitle, 1280, 720);
 
-	Vector3 translate(4.1f, 2.6f, 0.8f);
+	Vector3 rotate(4.1f, 2.6f, 0.8f);
 	Vector3 scale{ 1.5f,5.2f,7.3f };
 	Matrix4x4 translateMatrix = MakeTranslateMatrix(translate);
 	Matrix4x4 scaleMatrix = MakeScaleMatrix(scale);
