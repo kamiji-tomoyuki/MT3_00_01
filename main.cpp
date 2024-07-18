6804d8ccc9577d467712cf653e88b459cd1bb598
kamiji-tomoyuki <k023g0115@g.neec.ac.jp>
2024-07-18 16:11:25 +0900

    temp

--- main.cpp
+++ main.cpp
@@ -528,7 +528,11 @@ float Length(const Vector3& v1, const Vector3& v2) {
 
 	return d;
 }
-
+float Length(const Vector3& v){
+	float result;
+	result = sqrtf(v.x * v.x + v.y * v.y + v.z * v.z);
+	return result;
+}
 ///////////////////////////////////////////////////////////////////////
 
 //描画用関数
@@ -858,27 +862,6 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 		//ImGui
 		ImGui::Begin("window");
-
-		ImGui::Text("+:%f, %f, %f", c.x, c.y, c.z);
-		ImGui::Text("-:%f, %f, %f", d.x, d.y, d.z);
-		ImGui::Text("*:%f, %f, %f", e.x, e.y, e.z);
-		ImGui::Text("/:%f, %f, %f", f.x, f.y, f.z);
-
-		ImGui::Text("rotate:\n%f,%f,%f,%f\n%f,%f,%f,%f\n%f,%f,%f,%f\n%f,%f,%f,%f\n",
-			rotateMatrix.m[0][0], rotateMatrix.m[0][1], rotateMatrix.m[0][2], rotateMatrix.m[0][3],
-			rotateMatrix.m[1][0], rotateMatrix.m[1][1], rotateMatrix.m[1][2], rotateMatrix.m[1][3],
-			rotateMatrix.m[2][0], rotateMatrix.m[2][1], rotateMatrix.m[2][2], rotateMatrix.m[2][3],
-			rotateMatrix.m[3][0], rotateMatrix.m[3][1], rotateMatrix.m[3][2], rotateMatrix.m[3][3]);
-		ImGui::Text("+:\n%f,%f,%f,%f\n%f,%f,%f,%f\n%f,%f,%f,%f\n%f,%f,%f,%f\n",
-			m1.m[0][0], m1.m[0][1], m1.m[0][2], m1.m[0][3],
-			m1.m[1][0], m1.m[1][1], m1.m[1][2], m1.m[1][3],
-			m1.m[2][0], m1.m[2][1], m1.m[2][2], m1.m[2][3],
-			m1.m[3][0], m1.m[3][1], m1.m[3][2], m1.m[3][3]);
-		ImGui::Text("-:\n%f,%f,%f,%f\n%f,%f,%f,%f\n%f,%f,%f,%f\n%f,%f,%f,%f\n",
-			m2.m[0][0], m2.m[0][1], m2.m[0][2], m2.m[0][3],
-			m2.m[1][0], m2.m[1][1], m2.m[1][2], m2.m[1][3],
-			m2.m[2][0], m2.m[2][1], m2.m[2][2], m2.m[2][3],
-			m2.m[3][0], m2.m[3][1], m2.m[3][2], m2.m[3][3]);
 
 		ImGui::End();
 
